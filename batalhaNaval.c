2a18e1aeebc9583093312efbb05787607b58b374
AlexCorreaDJ <alexcorreagomess@hotmail.com>
2025-04-05 21:22:48 -0300

    nivel mestre 2.0

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,47 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Exibe qualquer matriz com 0 (não afetado) e 1 (afetado)
-void exibirMatriz(int matriz[5][5], const char* nome) {
-    printf("\nHabilidade: %s\n", nome);
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+#define TAM 10
+
+void limparTabuleiro(int matriz[TAM][TAM]) {
+    for(int i = 0; i < TAM; i++)
+        for(int j = 0; j < TAM; j++)
+            matriz[i][j] = 0;
+}
+
+void exibirTabuleiro(int matriz[TAM][TAM]) {
+    for(int i = 0; i < TAM; i++) {
+        for(int j = 0; j < TAM; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
+}
+
+// Cone: forma triangular com base para baixo
+void habilidadeCone(int matriz[TAM][TAM], int centroX, int centroY, int altura) {
+    for(int i = 0; i < altura; i++) {
+        for(int j = -i; j <= i; j++) {
+            int x = centroX + i;
+            int y = centroY + j;
+            if(x >= 0 && x < TAM && y >= 0 && y < TAM)
+                matriz[x][y] = 1;
+        }
+    }
+}
+
+// Cruz: afeta linha e coluna central
+void habilidadeCruz(int matriz[TAM][TAM], int centroX, int centroY, int alcance) {
+    for(int i = centroX - alcance; i <= centroX + alcance; i++) {
+        if(i >= 0 && i < TAM)
+            matriz[i][centroY] = 1;
+    }
+    for(int j = centroY - alcance; j <= centroY + alcance; j++) {
+        if(j >= 0 && j < TAM)
+            matriz[centroX][j] = 1;
+    }
+}
+
+// Octaedro (forma de losango baseado em distância de Manhattan)
+void habilidadeOctaedro(int matriz[TAM][TAM], int centroX, int centroY, int alcance) {
+    for(int i = 0; i < TAM; i++) {
+        for(int j = 0; j < TAM; j++) {
+            if(abs(i - centroX) + abs(j - centroY) <= alcance)
+                matriz[i][j] = 1;
+        }
+    }
 }
 
 int main() {
-    int cone[5][5] = {0};
-    int cruz[5][5] = {0};
-    int octaedro[5][5] = {0};
+    int tabuleiro[TAM][TAM];
+    int centroX = 5, centroY = 5;
+    int escolha;
 
-    // Cone (formato em pirâmide invertida)
-    for (int i = 0; i < 3; i++) {
-        for (int j = 2 - i; j <= 2 + i; j++) {
-            cone[i][j] = 1;
-        }
+    printf("=== BATALHA NAVAL - HABILIDADES ESPECIAIS ===\n");
+    printf("1 - Cone\n2 - Cruz\n3 - Octaedro\n");
+    printf("Escolha a habilidade (1 a 3): ");
+    scanf("%d", &escolha);
+
+    limparTabuleiro(tabuleiro);
+
+    switch(escolha) {
+        case 1:
+            habilidadeCone(tabuleiro, centroX, centroY, 3);
+            printf("\nHabilidade: Cone (altura = 3)\n");
+            break;
+        case 2:
+            habilidadeCruz(tabuleiro, centroX, centroY, 3);
+            printf("\nHabilidade: Cruz (alcance = 3)\n");
+            break;
+        case 3:
+            habilidadeOctaedro(tabuleiro, centroX, centroY, 2);
+            printf("\nHabilidade: Octaedro (alcance = 2)\n");
+            break;
+        default:
+            printf("Opção inválida!\n");
+            return 0;
     }
 
-    // Cruz
-    for (int i = 0; i < 5; i++) {
-        cruz[2][i] = 1; // linha do meio
-        cruz[i][2] = 1; // coluna do meio
-    }
-
-    // Octaedro (diamante)
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (abs(i - 2) + abs(j - 2) <= 1) {
-                octaedro[i][j] = 1;
-            }
-        }
-    }
-
-    // Exibir os padrões
-    exibirMatriz(cone, "Cone");
-    exibirMatriz(cruz, "Cruz");
-    exibirMatriz(octaedro, "Octaedro");
+    exibirTabuleiro(tabuleiro);
 
     return 0;
 }
